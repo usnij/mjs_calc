bb7a49011969f86d082e047013c60fb58507b6f7
jihunsong <songjihun1080@gmail.com>
2023-04-10 14:09:47 +0900

    error message occur when input other types excpet int

--- calc.py
+++ calc.py
@@ -1,25 +1,31 @@
 # get two integer parameters
 # return sum
 def plus(x, y):
-    return x+y
+    return x + y
+
 
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:        
+    while check >= 1:
         print("0: exit, 1: plus")
-        check = int(input())
-        if check == 1:
-            print("First Number")
-            x = int(input())
-            print("Second Number")
-            y = int(input())
-            print("answer : ", plus(x,y))
-        elif check > 1:
-            print("Unsupported")
+        try:
+            check = int(input())
+        except:
+            print("error, input integer please")
         else:
-            print("Thank you")
+            if check == 1:
+                print("First Number")
+                x = int(input())
+                print("Second Number")
+                y = int(input())
+                print("answer : ", plus(x, y))
+            elif check > 1:
+                print("Unsupported")
+            else:
+                print("Thank you")
+
 
 if __name__ == "__main__":
     main()
